e74a0caf04dd51a6798af02e75c4260cd542ec58
Pasha <majorovpaul@gmail.com>
2023-02-27 15:39:32 +0300

    fix3

--- JS homework/Объекты дз/задание_4.js
+++ JS homework/Объекты дз/задание_4.js
@@ -30,25 +30,22 @@ function ElcetroDevices(location) {
   this.location = location;
   this.condition = recent;
 }
-function Notebooks(location, condition, price, drivers, oc) {
+function Notebooks(location, condition, price, oc) {
   this.location = location;
   this.condition = condition;
   this.price = price;
-  this.drivers = drivers;
-  this.oc = "Linux";
+  this.oc = oc;
 }
-function Computers(location, condition, price, drivers, oc) {
+function Computers(location, condition, price, oc) {
   this.location = location;
   this.condition = condition;
   this.price = price;
-  this.drivers = drivers;
-  this.oc = "Windows";
+  this.oc = oc;
 }
-function Printers(location, condition, drivers, price, multicolor) {
+function Printers(location, condition, price, multicolor) {
   this.location = location;
   this.condition = condition;
   this.price = price;
-  this.drivers = drivers;
   this.multicolor = multicolor;
 }
 
@@ -69,4 +66,37 @@ Printers.prototype.ThereIsConntection = function () {
 };
 Printers.prototype.ThereIsNoConntection = function () {
   console.log("this printer is not connected with device");
+};
+
+Computers.prototype.HaveSameOc = function () {
+  console.log("these computers have some oc");
+};
+
+Computers.prototype.HaveNotSameOc = function () {
+  console.log("these computers have not some oc");
+};
+
+Notebooks.prototype.HaveSamePrice = function () {
+  console.log("these notebooks have some price");
+};
+
+Notebooks.prototype.HaveNotSamePrice = function () {
+  console.log("these notebooks have not some price");
+};
+
+const samsung = new Notebooks("office", "recent", "expensive", "Linux");
+const acer = new Computers("office", "recent", "cheap", "Windows");
+const hp = new Printers("office", "recent", "cheap", "colored");
+
+samsung.cpu = "Corei5";
+samsung.card = "AMD";
+
+acer.disk = "SSD";
+acer.drive = "no";
+
+hp.name = "LazerJet";
+hp.year = 2023;
+
+samsung.isPrinterConnected = function () {
+  console.log("this notebook is not connected with ${hp.name}");
 };
